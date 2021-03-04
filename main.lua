da0d94387f0aa60b238bd1232745b0b856628350
Dylan <chaodp2021@yahoo.com>
2021-03-04 12:00:08 -0500

    Added first 3 images

--- main.lua
+++ main.lua
@@ -1,11 +1,16 @@
 function love.load()
-    print("Hello, console")
+    x = 0
+    gl = love.graphics.newImage('pixil-frame-0 (14).png')
+    rl = love.graphics.newImage('pixil-frame-0 (15).png')
+    bl = love.graphics.newImage('pixil-frame-0 (16).png')
 end
 
 function love.update(dt)
-
+    x = x + 1
 end
 
 function love.draw()
-    love.graphics.print("Git 'er done", 50, 30)
+    love.graphics.draw(gl, 500, 300)
+    love.graphics.draw(rl, 500, 350)
+    love.graphics.draw(bl, 500, 400)
 end
